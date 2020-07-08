9dc94e9a8ab466c7136f181540ce6e0d2a38e53b
Max Zhou <Max.Zhou@nike.com>
2020-07-08 17:59:17 +0800

    Update webdriver.py

---
+++ study/webdriver.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+#driver = webdriver.Firefox()
+
+driver.get('https://www.baidu.com/')
+
+#element_keyword = driver.find_element_by_id('kw')
+
+#element_keyword.send_keys('宋曲')
+
+element_search_button = driver.find_element_by_id('su')
+
+print(element_search_button)
