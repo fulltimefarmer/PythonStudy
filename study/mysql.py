808ccafb7327a6ca5e458c853f26ae59c17bde3c
Max Zhou <Max.Zhou@nike.com>
2020-07-09 15:08:42 +0800

    Create mysql.py

---
+++ study/mysql.py
@@ -0,0 +1,30 @@
+import pymysql
+
+conn = pymysql.connect(
+    host='localhost',
+    port=3306,
+    user='root',
+    password='',
+    db='auth',
+    charset='utf8'
+)
+
+cursor = conn.cursor()
+
+sql = 'select * from gcsc_user'
+
+try:
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for row in results:
+        id = row[0]
+        username = row[2]
+        displayName = row[5]
+        createDate = row[7]
+
+        print("id=%s, username=%s, displayName=%s, createDate=%s" % (id, username, displayName, createDate))
+except e:
+    print(str(e))
+finally:
+    cursor.close()
+    conn.close()
