ed4970b2a2449b8573ff586f96ff37f327ba59d3
Max Zhou <Max.Zhou@nike.com>
2020-07-14 15:27:21 +0800

    Update web.py

--- study/web.py
+++ study/web.py
@@ -1,6 +1,12 @@
 import requests
+import json
 
-resp = requests.get('https://github.com/timeline.json')
+url = "https://scapi-qa.nike.com/move/auth/v1/public/get_token_by_pwd"
+headers = {'Content-Type': 'application/json'}
+data = json.dumps({"username":"admin", "password":"1234", "projectId":1})
+
+#resp = requests.get('https://github.com/timeline.json')
+resp = requests.post(url=url, data=data, headers=headers)
 
 resp.encoding = 'utf-8'
 isOk = resp.ok
