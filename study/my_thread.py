1bc1b86017d86718bbc97c8f05e74042b9fd377d
zhoujun <zhoujun9633@163.com>
2020-07-29 22:50:20 +0800

    Update my_thread.py

--- study/my_thread.py
+++ study/my_thread.py
@@ -1,23 +1,22 @@
 from concurrent.futures import ThreadPoolExecutor
 import time
 
+tasks=[]
 
 def spider(page):
     time.sleep(page)
     print(f"crawl task{page} finished")
     return page
 
-with ThreadPoolExecutor(max_workers=5) as t: 
-    task1 = t.submit(spider, 1)
-    task2 = t.submit(spider, 2) 
-    task3 = t.submit(spider, 3)
+with ThreadPoolExecutor(max_workers=5) as executor:
+    tasks=[]
+    for i in range(6,9):
+        future = executor.submit(spider, i)
+        tasks.append(future)
 
-    print(f"task1: {task1.done()}") 
-    print(f"task2: {task2.done()}")
-    print(f"task3: {task3.done()}")
-
-    time.sleep(4)
-    print(f"task1: {task1.done()}")
-    print(f"task2: {task2.done()}")
-    print(f"task3: {task3.done()}")
-    print(task1.result())  # get result
+    while len(tasks) > 0:
+        for task in tasks:
+            #print(f"task: {task.done()}")
+            if task.done():
+                print(task.result())
+                tasks.remove(task)
