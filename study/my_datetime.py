b549e8fa37608ccb2bfab0e15071a7086cfd8003
Max Zhou <Max.Zhou@nike.com>
2020-08-04 14:37:22 +0800

    datetime;

--- study/my_datetime.py
+++ study/my_datetime.py
@@ -5,14 +5,28 @@ ticks = time.time()
 print ("当前时间戳为:", ticks)
 
 d1 = datetime.datetime.now()
-print(d1)
+ts1 = d1.timestamp()
 ts1 = d1.timestamp()
 print(ts1)
-d2 = d1 + datetime.timedelta(hours=1)
-print(d2)
+d2 = d1 + datetime.timedelta(minutes=1)
+ts2 = d2.timestamp()
 ts2 = d2.timestamp()
 print(ts2)
-print(ts2 - ts1)
+d3 = d1 + datetime.timedelta(minutes=61)
+ts3 = d3.timestamp()
+print(ts3)
+
+test1 = ts2 - ts1
+if test1 > 3600:
+    print(False)
+else:
+    print(True)
+
+test2 = ts3 - ts1
+if test2 > 3600:
+    print(False)
+else:
+    print(True)
 
 # interval = d2 - d1
 # interval_days = interval.days
