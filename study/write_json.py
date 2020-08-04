ee413dc59c3b01309d01b0ca8bb7dd0447888dd0
Max Zhou <Max.Zhou@nike.com>
2020-08-04 13:55:13 +0800

    write json;

--- study/write_json.py
+++ study/write_json.py
@@ -1,2 +1,17 @@
+import os
+import datetime
+import time
 import json
 
+file_path = '..'+ os.sep + 'file' + os.sep + 'json' + os.sep + 'prepost.json'
+
+dict_data = []
+
+with open(file_path, 'r') as file:
+    dict_data = json.load(file)
+    print(dict_data)
+
+with open(file_path, 'w') as file:
+    str_time = datetime.datetime.now().strftime('%Y%m%d%H%M')
+    dict_data['update_time'] = str_time
+    json.dump(dict_data, file)
